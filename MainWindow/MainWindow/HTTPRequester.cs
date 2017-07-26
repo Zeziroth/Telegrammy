052701819c1922871fc1af37ec3a97529fe463fb
Glumi <pasquale.b@hotmail.de>
2017-07-26 21:09:01 +0200

    HTTPRequester hinzugefügt

---
+++ MainWindow/MainWindow/HTTPRequester.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainWindow
+{
+    public static class HTTPRequester
+    {
+        private static WebClient client = null;
+        private static void Init()
+        {
+            if (client == null)
+            {
+                client = new WebClient();
+                client.Proxy = null;
+            }
+        }
+
+        public static string SimpleRequest(string url)
+        {
+            Init();
+            return client.DownloadString(url);
+        }
+    }
+}
