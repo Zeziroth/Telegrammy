ac198b5776cb87f289b3e04211b06b3844bb0902
Glumi <pasquale.b@hotmail.de>
2017-10-03 12:14:53 +0200

    Addes usings

--- MainWindow/MainWindow/HTTPRequester.cs
+++ MainWindow/MainWindow/HTTPRequester.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.IO;
 using System.Net;
-
+using System.Text;
 namespace MainWindow
 {
     public static class HTTPRequester
